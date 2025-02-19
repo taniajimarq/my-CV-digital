7553e1b66e92e2bfd83f17fbccebbfd8c7455df8
TaniaJimarq <taniajimarq.utj@gmail.com>
2025-02-19 13:23:13 -0600

    Adecuaciónes para el despliegue en vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	webpack: (config, { isServer }) => {
+		config.module.rules.push({
+			test: /\.html$/,
+			use: 'html-loader',
+		});
+		if (!isServer)
+			config.resolve.fallback = {
+				fs: false,
+				path: false,
+				os: false,
+			};
+
+		return config;
+	},
 };
 
 export default nextConfig;
